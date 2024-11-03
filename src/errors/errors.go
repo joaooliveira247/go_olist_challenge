55484370b0c604d8c6000e8af6bb14353426e451
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-02 21:00:36 -0300

    :sparkles: add errors

---
+++ src/errors/errors.go
@@ -0,0 +1,12 @@
+package errors
+
+import "fmt"
+
+type BaseError struct {
+	Msg      string
+	Resource string
+}
+
+func (error *BaseError) Error() string {
+	return fmt.Sprintf("%s %s", error.Resource, error.Msg)
+}
