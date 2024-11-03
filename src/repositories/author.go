c2215daefa916cc34968a30eab5915be55a32657
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-03 17:41:16 -0300

    :sparkles: add authorRepository struct

--- src/repositories/author.go
+++ src/repositories/author.go
@@ -3,8 +3,13 @@ package repositories
 import (
 	"github.com/google/uuid"
 	"github.com/joaooliveira247/go_olist_challenge/src/models"
+	"gorm.io/gorm"
 )
 
 type AuthorRepository interface {
 	Create(author *models.Author) (uuid.UUID, error)
+}
+
+type authorRepository struct {
+	db *gorm.DB
 }
