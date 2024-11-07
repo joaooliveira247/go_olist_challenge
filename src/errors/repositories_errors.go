e114d8ab9504a93343624965d6b327db93af96e6
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-07 19:02:06 -0300

    :sparkles: add AuthorGenericError

--- src/errors/repositories_errors.go
+++ src/errors/repositories_errors.go
@@ -4,4 +4,7 @@ type AlreadyExists struct {
 	BaseError
 }
 
-var AuthorAlreadyExists = AlreadyExists{BaseError{"author", "already exists"}}
+var (
+	AuthorAlreadyExists = AlreadyExists{BaseError{"author", "already exists"}}
+	AuthorGenericError  = GenericError{BaseError{"author", "generic error"}}
+)
