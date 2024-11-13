a78e36f5285de73119c92a26597ca342bd1d3a51
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-13 20:27:34 -0300

    :fire: remove validate author methor

--- src/models/author.go
+++ src/models/author.go
@@ -2,17 +2,9 @@ package models
 
 import (
 	"github.com/google/uuid"
-	"github.com/joaooliveira247/go_olist_challenge/src/errors"
 )
 
 type Author struct {
 	ID   uuid.UUID `json:"id,omitempty" gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	Name string    `json:"name,omitempty" binding:"required,min=2" gorm:"type:varchar(255);column:name;unique;not null"`
-}
-
-func (author *Author) validate() error {
-	if author.Name == "" {
-		return &errors.FieldNameInvalid
-	}
-	return nil
 }
