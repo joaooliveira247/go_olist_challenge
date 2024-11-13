fd64c6dfd3ccb56e3e67e1c2178f6232658447ff
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-13 20:28:42 -0300

    :sparkles: add NotFound struct

--- src/errors/repositories_errors.go
+++ src/errors/repositories_errors.go
@@ -4,6 +4,10 @@ type AlreadyExists struct {
 	BaseError
 }
 
+type NotFound struct {
+	BaseError
+}
+
 var (
 	AuthorAlreadyExists = AlreadyExists{BaseError{"author", "already exists"}}
 	AuthorGenericError  = GenericError{BaseError{"author", "generic error"}}
