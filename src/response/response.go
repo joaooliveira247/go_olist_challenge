c4cb10e2573c98f1bd07531ff0540aa2db44366a
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-14 21:36:10 -0300

    :sparkles: add UnableFetchEntity

--- src/response/response.go
+++ src/response/response.go
@@ -15,4 +15,5 @@ var (
 	InvalidRequestBody = Response{http.StatusUnprocessableEntity, gin.H{"message": "request body invalid"}}
 	UnableConnectDatabase = Response{http.StatusInternalServerError, gin.H{"message": "unable to connect to database"}}
 	UnableCreateEntity    = Response{http.StatusInternalServerError, gin.H{"message": "unable to create entity"}}
+	UnableFetchEntity     = Response{http.StatusInternalServerError, gin.H{"message": "unable to fetch entity"}}
 )
