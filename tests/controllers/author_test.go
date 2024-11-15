3b734b7a9b124f6441334a6c0faf0abdd94a8df6
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-14 21:25:29 -0300

    :white_check_mark: add controller TestCreateSuccess

---
+++ tests/controllers/author_test.go
@@ -0,0 +1,41 @@
+package controllers_test
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/joaooliveira247/go_olist_challenge/src/controllers"
+	"github.com/joaooliveira247/go_olist_challenge/src/models"
+	"github.com/joaooliveira247/go_olist_challenge/tests/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateSuccess(t *testing.T) {
+	mockRepository := new(mocks.AuthorRepository)
+	expectedID := uuid.New()
+	author := models.Author{
+		Name: "Luciano Ramalho",
+	}
+	mockRepository.On("Create", &author).Return(expectedID, nil)
+
+	controller := controllers.NewAuthorController(mockRepository)
+
+	w := httptest.NewRecorder()
+	gin.SetMode(gin.TestMode)
+	c, _ := gin.CreateTestContext(w)
+
+	body := `{"name": "Luciano Ramalho"}`
+
+	c.Request, _ = http.NewRequest(http.MethodPost, "/authors/", bytes.NewBufferString(body))
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	controller.CreateAuthor(c)
+
+	assert.Equal(t, http.StatusCreated, w.Code)
+	assert.JSONEq(t, fmt.Sprintf(`{"id": "%s"}`, expectedID), w.Body.String())
+}
