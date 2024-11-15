ebb3e5690bf5d49730965217e51fb094b4705171
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-14 21:33:04 -0300

    :sparkles: add CreateAuthor method in AuthorController

--- src/controllers/author.go
+++ src/controllers/author.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joaooliveira247/go_olist_challenge/src/models"
 	"github.com/joaooliveira247/go_olist_challenge/src/repositories"
+	"github.com/joaooliveira247/go_olist_challenge/src/response"
 )
 
 type AuthorController struct {
@@ -10,4 +15,23 @@ type AuthorController struct {
 
 func NewAuthorController(repo repositories.AuthorRepository) *AuthorController {
 	return &AuthorController{repo}
+}
+
+func (ctrl *AuthorController) CreateAuthor(ctx *gin.Context) {
+	var author models.Author
+
+	if err := ctx.ShouldBindJSON(&author); err != nil {
+		ctx.JSON(response.InvalidRequestBody.StatusCode, response.InvalidRequestBody.Message)
+		return
+	}
+
+	id, err := ctrl.repository.Create(&author)
+
+	if err != nil {
+		ctx.JSON(response.UnableCreateEntity.StatusCode, response.UnableCreateEntity.Message)
+		return
+	}
+
+	ctx.JSON(http.StatusCreated, gin.H{"id": id})
+	return
 }
