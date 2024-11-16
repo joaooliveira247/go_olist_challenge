786c93665985cb008f4644ec3fa1c38968d9e378
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-16 11:36:20 -0300

    :sparkles: add InvalidID response

--- src/response/response.go
+++ src/response/response.go
@@ -12,7 +12,9 @@ type Response struct {
 }
 
 var (
-	InvalidRequestBody = Response{http.StatusUnprocessableEntity, gin.H{"message": "request body invalid"}}
+	InvalidRequestBody    = Response{http.StatusUnprocessableEntity, gin.H{"message": "request body invalid"}}
+	InvalidID             = Response{http.StatusUnprocessableEntity, gin.H{"message": "invalid id"}}
+	AuthorAlreadyExists   = Response{http.StatusConflict, gin.H{"message": "author already exists"}}
 	UnableConnectDatabase = Response{http.StatusInternalServerError, gin.H{"message": "unable to connect to database"}}
 	UnableCreateEntity    = Response{http.StatusInternalServerError, gin.H{"message": "unable to create entity"}}
 	UnableFetchEntity     = Response{http.StatusInternalServerError, gin.H{"message": "unable to fetch entity"}}
