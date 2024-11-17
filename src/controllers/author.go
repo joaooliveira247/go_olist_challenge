0c3e9202aeeff525346f91687066e82aa16d50fb
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-17 20:54:08 -0300

    :sparkles: add GetAuthorByName method in AuthorController

--- src/controllers/author.go
+++ src/controllers/author.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
+	"github.com/joaooliveira247/go_olist_challenge/src/dto"
 	custom "github.com/joaooliveira247/go_olist_challenge/src/errors"
 	"github.com/joaooliveira247/go_olist_challenge/src/models"
 	"github.com/joaooliveira247/go_olist_challenge/src/repositories"
@@ -71,5 +72,24 @@ func (ctrl *AuthorController) GetAuthorByID(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, author)
+	return
+}
+
+func (ctrl *AuthorController) GetAuthorByName(ctx *gin.Context) {
+	var queryParam dto.AuthorQueryParam
+
+	if ctx.ShouldBindQuery(&queryParam) != nil {
+		ctx.JSON(response.InvalidParam.StatusCode, response.InvalidParam.Message)
+		return
+	}
+
+	authors, err := ctrl.repository.GetByName(queryParam.Name)
+
+	if err != nil {
+		ctx.JSON(response.UnableFetchEntity.StatusCode, response.UnableFetchEntity.Message)
+		return
+	}
+
+	ctx.JSON(http.StatusOK, authors)
 	return
 }
