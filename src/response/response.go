adbbcb6f12f0ed4d098510db68df6f748f3f68c4
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-17 21:47:40 -0300

    :sparkles: add AuthorNotFound response

--- src/response/response.go
+++ src/response/response.go
@@ -16,6 +16,7 @@ var (
 	InvalidID             = Response{http.StatusBadRequest, gin.H{"message": "invalid id"}}
 	InvalidParam          = Response{http.StatusBadRequest, gin.H{"message": "invalid query param"}}
 	AuthorAlreadyExists   = Response{http.StatusConflict, gin.H{"message": "author already exists"}}
+	AuthorNotFound        = Response{http.StatusNotFound, gin.H{"message": "author not found"}}
 	UnableConnectDatabase = Response{http.StatusInternalServerError, gin.H{"message": "unable to connect to database"}}
 	UnableCreateEntity    = Response{http.StatusInternalServerError, gin.H{"message": "unable to create entity"}}
 	UnableFetchEntity     = Response{http.StatusInternalServerError, gin.H{"message": "unable to fetch entity"}}
