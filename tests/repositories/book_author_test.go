8351597c4f862bc2aede37207ff69206dfaf2f27
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-20 08:55:09 -0300

    :white_check_mark: add repository TestCreateBookAuthorSuccess

---
+++ tests/repositories/book_author_test.go
@@ -0,0 +1,37 @@
+package repositories_test
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"github.com/joaooliveira247/go_olist_challenge/src/models"
+	"github.com/joaooliveira247/go_olist_challenge/src/repositories"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateBookAuthorSuccess(t *testing.T) {
+	gormDB, mock := SetupMockDB()
+
+	defer func() {
+		db, _ := gormDB.DB()
+		db.Close()
+	}()
+
+	bookID := uuid.New()
+	authorID := uuid.New()
+
+	mock.ExpectBegin()
+	mock.ExpectExec(
+		regexp.QuoteMeta(
+			`INSERT INTO "book_authors" ("book_id","author_id") VALUES ($1,$2)`,
+		),
+	).WithArgs(bookID, authorID).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	repository := repositories.NewBookAuthorRepository(gormDB)
+	err := repository.Create(&models.BookAuthor{BookID: bookID, AuthorID: authorID})
+
+	assert.Nil(t, err)
+}
