30fbdeb1c52ba711f6f6bd3d66441c94daf8fb07
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-20 10:16:49 -0300

    :sparkles: add BookAuthor model

---
+++ src/models/book_author.go
@@ -0,0 +1,10 @@
+package models
+
+import "github.com/google/uuid"
+
+type BookAuthor struct {
+	BookID   uuid.UUID `gorm:"primaryKey;column:book_id"`
+	Book     Book      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:BookID;"`
+	AuthorID uuid.UUID `gorm:"primaryKey;column:author_id"`
+	Author   Author    `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;foreignKey:AuthorID;"`
+}
