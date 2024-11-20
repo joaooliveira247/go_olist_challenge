8d75d0194614703b879eaced00397f1c44f3381b
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-20 10:18:39 -0300

    :sparkles: add bookAuthorRepository struct

--- src/repositories/book_author.go
+++ src/repositories/book_author.go
@@ -1,7 +1,10 @@
 package repositories
 
 import (
+	"errors"
+
 	"github.com/google/uuid"
+	"gorm.io/gorm"
 
 	"github.com/joaooliveira247/go_olist_challenge/src/models"
 )
@@ -10,3 +13,8 @@ type BookAuthorRepository interface {
 	Create(relationship *models.BookAuthor) error
 	Delete(bookID uuid.UUID) error
 }
+
+type bookAuthorRepository struct {
+	db *gorm.DB
+}
+
