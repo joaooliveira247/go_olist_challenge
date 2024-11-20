2baa474493a4fcba1a0a0bc860b41c30131c3cb3
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-20 09:02:37 -0300

    :white_check_mark: add repository TestCreateBookAuthorReturnGenericError

--- tests/repositories/book_author_test.go
+++ tests/repositories/book_author_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
+	"github.com/joaooliveira247/go_olist_challenge/src/errors"
 	"github.com/joaooliveira247/go_olist_challenge/src/models"
 	"github.com/joaooliveira247/go_olist_challenge/src/repositories"
 	"github.com/stretchr/testify/assert"
@@ -34,4 +35,30 @@ func TestCreateBookAuthorSuccess(t *testing.T) {
 	err := repository.Create(&models.BookAuthor{BookID: bookID, AuthorID: authorID})
 
 	assert.Nil(t, err)
+}
+
+func TestCreateBookAuthorReturnGenericError(t *testing.T) {
+	gormDB, mock := SetupMockDB()
+
+	defer func() {
+		db, _ := gormDB.DB()
+		db.Close()
+	}()
+
+	bookID := uuid.New()
+	authorID := uuid.New()
+
+	mock.ExpectBegin()
+	mock.ExpectExec(
+		regexp.QuoteMeta(
+			`INSERT INTO "book_authors" ("book_id","author_id") VALUES ($1,$2)`,
+		),
+	).WithArgs(bookID, authorID).WillReturnError(&errors.AuthorGenericError)
+	mock.ExpectRollback()
+
+	repository := repositories.NewBookAuthorRepository(gormDB)
+	err := repository.Create(&models.BookAuthor{BookID: bookID, AuthorID: authorID})
+
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, &errors.AuthorGenericError)
 }
