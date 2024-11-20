56f86a3f3364b24d792a199483a7fa6d4e8c1880
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-20 13:34:35 -0300

    :sparkles: add BookAuthorGenericError

--- src/errors/repositories_errors.go
+++ src/errors/repositories_errors.go
@@ -11,6 +11,7 @@ type NotFound struct {
 var (
 	AuthorAlreadyExists       = AlreadyExists{BaseError{"author", "already exists"}}
 	AuthorGenericError        = GenericError{BaseError{"author", "generic error"}}
+	BookAuthorGenericError    = GenericError{BaseError{"book_author", "generic error"}}
 	AuthorNotFound            = NotFound{BaseError{"author", "not found"}}
 	RelationshipAlreadyExists = AlreadyExists{BaseError{"relationship", "already exists"}}
 )
