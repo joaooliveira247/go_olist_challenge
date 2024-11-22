0c6ada211005175849b0fee7eda605b03044e296
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-22 10:07:39 -0300

    :white_check_mark: add repository TestCreateBookReturnAlredyExists

--- tests/repositories/book_test.go
+++ tests/repositories/book_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/google/uuid"
+	"github.com/joaooliveira247/go_olist_challenge/src/errors"
 	"github.com/joaooliveira247/go_olist_challenge/src/models"
 	"github.com/joaooliveira247/go_olist_challenge/src/repositories"
 	"github.com/stretchr/testify/assert"
@@ -46,4 +47,34 @@ func TestCreateBookSuccess(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.Equal(t, bookID, id)
+}
+
+func TestCreateBookReturnAlredyExists(t *testing.T) {
+	gormDB, mock := SetupMockDB()
+
+	defer func() {
+		db, _ := gormDB.DB()
+		db.Close()
+	}()
+
+	bookID := uuid.New()
+
+	book := models.Book{
+		Title:           "The Rust Programming Language",
+		Edition:         1,
+		PublicationYear: 2018,
+	}
+
+	mock.ExpectQuery(
+		regexp.QuoteMeta(
+			`SELECT * FROM "books" WHERE "books"."title" = $1 AND "books"."edition" = $2 AND "books"."publication_year" = $3 ORDER BY "books"."id" LIMIT $4`,
+		),
+	).WithArgs(book.Title, book.Edition, book.PublicationYear, 1).WillReturnRows(sqlmock.NewRows([]string{"id", "title", "edition", "publication_year"}).AddRow(bookID, book.Title, book.Edition, book.PublicationYear))
+
+	repository := repositories.NewBookRepository(gormDB)
+	id, err := repository.Create(&book)
+
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, &errors.BookAlreadyExists)
+	assert.Equal(t, uuid.Nil, id)
 }
