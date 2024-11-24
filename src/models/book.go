96506216a5a256dad58350101d49dc1b975e7eab
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-24 10:33:32 -0300

    :sparkles: add BookOut model

--- src/models/book.go
+++ src/models/book.go
@@ -1,10 +1,18 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/lib/pq"
+)
 
 type Book struct {
 	ID              uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	Title           string    `gorm:"type:varchar(255);not null;column:title"`
 	Edition         uint8     `gorm:"type:smallint;column:edition"`
 	PublicationYear uint      `gorm:"type:smallint;column:publication_year"`
+}
+
+type BookOut struct {
+	Book
+	AuthorsName pq.StringArray `gorm:"type:text[];column:authors"`
 }
