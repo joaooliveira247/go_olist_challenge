d414731982f80922c7b60cf606dd537fd09d53bc
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-24 21:17:24 -0300

    :sparkles: add NewMockBooks

--- tests/repositories/common_functions_test.go
+++ tests/repositories/common_functions_test.go
@@ -4,7 +4,9 @@ import (
 	"log"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
 	"github.com/joaooliveira247/go_olist_challenge/src/models"
+	"github.com/lib/pq"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -28,5 +30,46 @@ func NewMockBook() *models.Book {
 		Title:           "The Rust Programming Language",
 		Edition:         1,
 		PublicationYear: 2018,
+	}
+}
+
+func NewMockBooks() []models.BookOut {
+	return []models.BookOut{
+		{
+			Book: models.Book{
+				ID:              uuid.New(),
+				Title:           "The Rust Programming Language",
+				Edition:         1,
+				PublicationYear: 2018,
+			},
+			AuthorsName: pq.StringArray{"Carol Nichols", "Steve Klabnik"},
+		},
+		{
+			Book: models.Book{
+				ID:              uuid.New(),
+				Title:           "The Go Programming Language",
+				Edition:         2,
+				PublicationYear: 2015,
+			},
+			AuthorsName: pq.StringArray{"Alan A. A. Donovan", "Brian W. Kernighan"},
+		},
+		{
+			Book: models.Book{
+				ID:              uuid.New(),
+				Title:           "Python Fluente",
+				Edition:         1,
+				PublicationYear: 2015,
+			},
+			AuthorsName: pq.StringArray{"Luciano Ramalho"},
+		},
+		{
+			Book: models.Book{
+				ID:              uuid.New(),
+				Title:           "Python Fluente",
+				Edition:         2,
+				PublicationYear: 2022,
+			},
+			AuthorsName: pq.StringArray{"Luciano Ramalho"},
+		},
 	}
 }
