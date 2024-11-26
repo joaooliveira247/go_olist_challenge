f4e04974a7e635a8d3f503ab70cf19515bf42225
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-26 10:36:40 -0300

    :sparkles: add NewBookController func

--- src/controllers/book.go
+++ src/controllers/book.go
@@ -5,3 +5,7 @@ import "github.com/joaooliveira247/go_olist_challenge/src/repositories"
 type BookController struct {
 	repository repositories.BookRepository
 }
+
+func NewBookController(repo repositories.BookRepository) *BookController {
+	return &BookController{repo}
+}
