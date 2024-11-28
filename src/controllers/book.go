286129b742e9524afe3617c86bfbaff79ae24fce
joaooliveira247 <joaooliveira247@gmail.com>
2024-11-28 09:27:42 -0300

    :sparkles: add bookAuthorRepository in BookController struct

--- src/controllers/book.go
+++ src/controllers/book.go
@@ -1,11 +1,10 @@
 package controllers
 
-import "github.com/joaooliveira247/go_olist_challenge/src/repositories"
+import (
+	"github.com/joaooliveira247/go_olist_challenge/src/repositories"
+)
 
 type BookController struct {
-	repository repositories.BookRepository
-}
-
-func NewBookController(repo repositories.BookRepository) *BookController {
-	return &BookController{repo}
+	bookRepository repositories.BookRepository
+	bookAuthorRepository repositories.BookAuthorRepository
 }
