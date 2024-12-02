ca72ad73ab7e584c5e559ab48f00b4f074b71b60
joaooliveira247 <joaooliveira247@gmail.com>
2024-12-01 21:12:42 -0300

    :sparkles: add BookQueryParam struct

--- src/dto/query_params.go
+++ src/dto/query_params.go
@@ -3,3 +3,10 @@ package dto
 type AuthorQueryParam struct {
 	Name string `form:"name" binding:"min=2"`
 }
+
+type BookQueryParam struct {
+	Title           string `form:"title,omitempty" json:"title,omitempty"`
+	Edition         uint8  `form:"edition,omitempty" json:"edition,omitempty"`
+	PublicationYear uint   `form:"publicationYear,omitempty" json:"publication_year,omitempty"`
+	Author          string `form:"author,omitempty" json:"author,omitempty"`
+}
