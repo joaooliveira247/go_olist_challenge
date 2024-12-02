79fadfe9bcac6923211c3710699c373810837699
joaooliveira247 <joaooliveira247@gmail.com>
2024-12-02 19:20:05 -0300

    :recycle: remove Author field in BookQueryParam

--- src/dto/query_params.go
+++ src/dto/query_params.go
@@ -1,12 +1,33 @@
 package dto
 
+import (
+	"encoding/json"
+
+	"github.com/google/uuid"
+)
+
 type AuthorQueryParam struct {
 	Name string `form:"name" binding:"min=2"`
 }
 
 type BookQueryParam struct {
-	Title           string `form:"title,omitempty" json:"title,omitempty"`
-	Edition         uint8  `form:"edition,omitempty" json:"edition,omitempty"`
-	PublicationYear uint   `form:"publicationYear,omitempty" json:"publication_year,omitempty"`
-	Author          string `form:"author,omitempty" json:"author,omitempty"`
+	Title           string    `form:"title,omitempty" json:"title,omitempty"`
+	Edition         uint8     `form:"edition,omitempty" json:"edition,omitempty"`
+	PublicationYear uint      `form:"publicationYear,omitempty" json:"publication_year,omitempty"`
+}
+
+func (query *BookQueryParam) AsQuery() (map[string]interface{}, error) {
+	bjson, err := json.Marshal(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var queries map[string]interface{}
+
+	if err := json.Unmarshal(bjson, &queries); err != nil {
+		return nil, err
+	}
+
+	return queries, nil
 }
