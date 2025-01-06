13e9ff750d8c69840dbaf582f7b743cef4031c37
joaooliveira247 <joaooliveira247@gmail.com>
2025-01-06 18:09:23 -0300

    :white_check_mark: add db func TestCreateAllTablesReturnErrorGenericWhenCreateBookAuthorsTable

--- tests/db/manage_test.go
+++ tests/db/manage_test.go
@@ -159,3 +159,39 @@ func TestCreateAllTablesReturnErrorWhenCreateBookAuthorsTableAlreadyExists(t *te
 
 	assert.Error(t, err)
 }
+
+func TestCreateAllTablesReturnErrorGenericWhenCreateBookAuthorsTable(t *testing.T) {
+	gormDB, mock := mocks.SetupMockDB()
+
+	// Mock SELECT for "authors" table existence check
+	mock.ExpectQuery(regexp.QuoteMeta(
+		`SELECT count(*) FROM information_schema.tables WHERE table_schema = CURRENT_SCHEMA() AND table_name = $1 AND table_type = $2`,
+	)).WithArgs("authors", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
+
+	// Mock CREATE TABLE for "authors"
+	mock.ExpectExec(regexp.QuoteMeta(
+		`CREATE TABLE "authors" ("id" uuid DEFAULT gen_random_uuid(),"name" varchar(255) NOT NULL,PRIMARY KEY ("id"),CONSTRAINT "uni_authors_name" UNIQUE ("name"))`,
+	)).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery(regexp.QuoteMeta(
+		`SELECT count(*) FROM information_schema.tables WHERE table_schema = CURRENT_SCHEMA() AND table_name = $1 AND table_type = $2`,
+	)).WithArgs("books", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
+
+	mock.ExpectExec(regexp.QuoteMeta(
+		`CREATE TABLE "books" ("id" uuid DEFAULT gen_random_uuid(),"title" varchar(255) NOT NULL,"edition" smallint,"publication_year" smallint,PRIMARY KEY ("id"))`,
+	)).WillReturnResult(sqlmock.NewResult(1, 1))
+
+	mock.ExpectQuery(regexp.QuoteMeta(
+		`SELECT count(*) FROM information_schema.tables WHERE table_schema = CURRENT_SCHEMA() AND table_name = $1 AND table_type = $2`,
+	)).WithArgs("book_authors", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
+
+	// Mock CREATE TABLE for "book_authors"
+	mock.ExpectExec(regexp.QuoteMeta(
+		`CREATE TABLE "book_authors" ("book_id" uuid,"author_id" uuid,PRIMARY KEY ("book_id","author_id"),CONSTRAINT "fk_book_authors_book" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE CASCADE ON UPDATE CASCADE,CONSTRAINT "fk_book_authors_author" FOREIGN KEY ("author_id") REFERENCES "authors"("id") ON DELETE CASCADE ON UPDATE CASCADE)`,
+	)).WillReturnError(&errors.BookAuthorGenericError)
+
+	err := db.CreateTables(gormDB)
+
+	assert.Error(t, err)
+	assert.ErrorIs(t, err, &errors.BookAuthorGenericError)
+}
