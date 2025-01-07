2e56a7c57c8e50450066852a214f7de856626396
joaooliveira247 <joaooliveira247@gmail.com>
2025-01-07 17:30:54 -0300

    :sparkles: add func DeleteAllTables

--- src/db/manage.go
+++ src/db/manage.go
@@ -11,3 +11,19 @@ func CreateTables(db *gorm.DB) error {
 	}
 	return nil
 }
+
+func DeleteAllTables(db *gorm.DB) error {
+	rawQuery := `
+do $$ declare
+    r record;
+begin
+    for r in (select tablename from pg_tables where schemaname = 'public') loop
+        execute 'drop table if exists ' || quote_ident(r.tablename) || ' cascade';
+    end loop;
+end $$;
+		`
+	if err := db.Exec(rawQuery).Error; err != nil {
+		return err
+	}
+	return nil
+}
