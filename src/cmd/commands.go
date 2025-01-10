81eb5edf304dedcd20e8684722ae66af695a7e49
joaooliveira247 <joaooliveira247@gmail.com>
2025-01-09 21:15:46 -0300

    :sparkles: add cmd runAPI func

--- src/cmd/commands.go
+++ src/cmd/commands.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/gin-gonic/gin"
+	"github.com/joaooliveira247/go_olist_challenge/src/config"
 	"github.com/joaooliveira247/go_olist_challenge/src/db"
+	"github.com/joaooliveira247/go_olist_challenge/src/routes"
 	"github.com/urfave/cli/v3"
 )
 
@@ -32,5 +36,19 @@ func deleteTables(_ context.Context, cmd *cli.Command) error {
 		return err
 	}
 
+	return nil
+}
+
+func runAPI(_ context.Context, cmd *cli.Command) error {
+	api := gin.Default()
+	routes.RegistryRoutes(api)
+	port := config.APIPort
+	if cliPort := cmd.Int("port"); cliPort > 0 {
+		port = int(cliPort)
+	}
+
+	if err := api.Run(fmt.Sprintf(":%d", port)); err != nil {
+		return err
+	}
 	return nil
 }
