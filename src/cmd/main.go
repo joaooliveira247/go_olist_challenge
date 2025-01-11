cf2d80b2614a8be4643c27e7ecf1c29198129752
joaooliveira247 <joaooliveira247@gmail.com>
2025-01-11 15:48:09 -0300

    :sparkles: add main function

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/joaooliveira247/go_olist_challenge/src/config"
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/joaooliveira247/go_olist_challenge/src/config"
+)
 
 func init() {
 	config.LoadEnv()
+}
+
+func main() {
+	app := Gen()
+	if err := app.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
