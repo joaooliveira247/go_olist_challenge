03e99b40dd39f0b57ce300b62b3fe39d799ae3c6
joaooliveira247 <joaooliveira247@gmail.com>
2025-01-12 10:56:20 -0300

    :sparkles: add ParseAuthorsFromCSV

---
+++ src/utils/csv.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"encoding/csv"
+	"os"
+
+	"github.com/joaooliveira247/go_olist_challenge/src/models"
+)
+
+func ParseAuthorsFromCSV(path string, header bool) ([]models.Author, error) {
+	file, err := os.Open(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	reader := csv.NewReader(file)
+
+	lines, err := reader.ReadAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !header {
+		lines = lines[1:]
+	}
+
+	var authors []models.Author
+
+	for _, line := range lines {
+		author := models.Author{Name: line[0]}
+		authors = append(authors, author)
+	}
+
+	return authors, nil
+}
