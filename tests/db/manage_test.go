2754c35cc7c658029836eaac2f2330883051831d
joaooliveira247 <joaooliveira247@gmail.com>
2025-01-24 19:11:52 -0300

    :white_check_mark:fix manage tests 'cause change in table name book_authors -> book_author

--- tests/db/manage_test.go
+++ tests/db/manage_test.go
@@ -34,14 +34,14 @@ func TestCreateAllTablesSuccess(t *testing.T) {
 		`CREATE TABLE "books" ("id" uuid DEFAULT gen_random_uuid(),"title" varchar(255) NOT NULL,"edition" smallint,"publication_year" smallint,PRIMARY KEY ("id"))`,
 	)).WillReturnResult(sqlmock.NewResult(1, 1))
 
-	// Mock SELECT for "book_authors" table existence check
+	// Mock SELECT for "book_author" table existence check
 	mock.ExpectQuery(regexp.QuoteMeta(
 		`SELECT count(*) FROM information_schema.tables WHERE table_schema = CURRENT_SCHEMA() AND table_name = $1 AND table_type = $2`,
-	)).WithArgs("book_authors", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
+	)).WithArgs("book_author", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
 
-	// Mock CREATE TABLE for "book_authors"
+	// Mock CREATE TABLE for "book_author"
 	mock.ExpectExec(regexp.QuoteMeta(
-		`CREATE TABLE "book_authors" ("book_id" uuid,"author_id" uuid,PRIMARY KEY ("book_id","author_id"),CONSTRAINT "fk_book_authors_book" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE CASCADE ON UPDATE CASCADE,CONSTRAINT "fk_book_authors_author" FOREIGN KEY ("author_id") REFERENCES "authors"("id") ON DELETE CASCADE ON UPDATE CASCADE)`,
+		`CREATE TABLE "book_author" ("book_id" uuid,"author_id" uuid,PRIMARY KEY ("book_id","author_id"),CONSTRAINT "fk_book_author_book" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE CASCADE ON UPDATE CASCADE,CONSTRAINT "fk_book_author_author" FOREIGN KEY ("author_id") REFERENCES "authors"("id") ON DELETE CASCADE ON UPDATE CASCADE)`,
 	)).WillReturnResult(sqlmock.NewResult(1, 1))
 
 	err := db.CreateTables(gormDB)
@@ -153,7 +153,7 @@ func TestCreateAllTablesReturnErrorWhenCreateBookAuthorsTableAlreadyExists(t *te
 
 	mock.ExpectQuery(regexp.QuoteMeta(
 		`SELECT count(*) FROM information_schema.tables WHERE table_schema = CURRENT_SCHEMA() AND table_name = $1 AND table_type = $2`,
-	)).WithArgs("book_authors", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
+	)).WithArgs("book_author", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(1))
 
 	err := db.CreateTables(gormDB)
 
@@ -183,11 +183,11 @@ func TestCreateAllTablesReturnErrorGenericWhenCreateBookAuthorsTable(t *testing.
 
 	mock.ExpectQuery(regexp.QuoteMeta(
 		`SELECT count(*) FROM information_schema.tables WHERE table_schema = CURRENT_SCHEMA() AND table_name = $1 AND table_type = $2`,
-	)).WithArgs("book_authors", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
+	)).WithArgs("book_author", "BASE TABLE").WillReturnRows(sqlmock.NewRows([]string{"count"}).AddRow(0))
 
-	// Mock CREATE TABLE for "book_authors"
+	// Mock CREATE TABLE for "book_author"
 	mock.ExpectExec(regexp.QuoteMeta(
-		`CREATE TABLE "book_authors" ("book_id" uuid,"author_id" uuid,PRIMARY KEY ("book_id","author_id"),CONSTRAINT "fk_book_authors_book" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE CASCADE ON UPDATE CASCADE,CONSTRAINT "fk_book_authors_author" FOREIGN KEY ("author_id") REFERENCES "authors"("id") ON DELETE CASCADE ON UPDATE CASCADE)`,
+		`CREATE TABLE "book_author" ("book_id" uuid,"author_id" uuid,PRIMARY KEY ("book_id","author_id"),CONSTRAINT "fk_book_author_book" FOREIGN KEY ("book_id") REFERENCES "books"("id") ON DELETE CASCADE ON UPDATE CASCADE,CONSTRAINT "fk_book_author_author" FOREIGN KEY ("author_id") REFERENCES "authors"("id") ON DELETE CASCADE ON UPDATE CASCADE)`,
 	)).WillReturnError(&errors.BookAuthorGenericError)
 
 	err := db.CreateTables(gormDB)
